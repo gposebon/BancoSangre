947c9f9ceded195e0d6611d1b5fb9a2a0a0ba52b
tisoler <diegompaz@gmail.com>
2017-11-20 17:20:35 -0300

    Errores diseño

--- BancoSangre/Scripts/lanzamiento.js
+++ BancoSangre/Scripts/lanzamiento.js
@@ -1,1 +1,8 @@
 ﻿
+$(document).ready(function () {
+	$(".barraIngresoIzquierda").css({ 'right': '9%' });
+	$(".barraIngresoDerecha").css({ 'left': '49%' });
+	$("#logoBancoSangre").fadeIn(3000, function () {
+		$("#divLanzamiento").fadeOut(2000);
+	});
+});
